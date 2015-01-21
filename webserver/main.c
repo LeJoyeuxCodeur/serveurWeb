71a96d4db35162b54c3205a935b2a583bf813743
Bryan Vergauwen <bryan.vergauwen@hotmail.fr>
2015-01-21 14:27:22 +0100

    up

--- webserver/main.c
+++ webserver/main.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <stdio.h>
 
 int main(void){
 	int serveur = creer_serveur(8000);
@@ -11,13 +12,19 @@ int main(void){
 	int alt;
 	const char *intro = "Bienvenue, mortel, sur ce magnifique serveur d'echo\nIci ma gueule, quand tu parles, on te répond !\nCependant, tu auras la grosse impression de dialoguer... tout seul !\nHeureusement, tu connais ça, espèce de sans amis!\nTu peux t'en aller après avoir pris assez de vents dans la tête avec un ctrl + c\nSalut rémi sans amis!\n";
 	char buffer[256];
+	int tmp;
 	
 	while(1){
 		if((client = accept(serveur, NULL, NULL)) == -1)
 			perror("accept");
 		write(client, intro, strlen(intro));
-		while((alt = read(client, buffer, 256)))
+		while((alt = read(client, buffer, 256))){
 			write(client, buffer, alt);
+			
+			/* Detection fin de session ??
+			tmp = recv(serveur, buffer, sizeof(buffer), 0);
+			printf("%d", tmp); */
+		}
 	}
 	return 0;
 }
