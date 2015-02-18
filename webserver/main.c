496441d952bea2393315314754526f1b253030b5
ANTOINE DERANTON <derantoa@ayou10.iut-infobio.priv.univ-lille1.fr>
2015-02-18 10:26:55 +0100

    fdopen

--- webserver/main.c
+++ webserver/main.c
@@ -11,10 +11,10 @@
 int main(void){
 	int serveur = creer_serveur(8000);
 	int client;
-	int alt;
 	int pid;
+	FILE *file;
 	const char *intro = "Bienvenue, mortel, sur ce magnifique serveur d'echo\nIci ma gueule, quand tu parles, on te répond !\nCependant, tu auras la grosse impression de dialoguer... tout seul !\nHeureusement, tu connais ça, espèce de sans amis!\nTu peux t'en aller après avoir pris assez de vents dans la tête avec un ctrl + c\nSalut rémi sans amis!\n";
-	char buffer[256];
+	char buffer[1024];
 	
 	while(1){
 		if((client = accept(serveur, NULL, NULL)) == -1)
@@ -29,8 +29,9 @@ int main(void){
 		    if(close(client)==-1)
 		      perror("close");
 		  }else{
-		    while((alt = read(client, buffer, 256))){
-		      write(client, buffer, alt);
+		    file=fdopen(client, "w+");
+		    while(fgets(buffer, 1024, file)!=NULL){
+		      fprintf(file, "YouCantTestMyServer %s" ,buffer);
 		    }
 		    printf("Client déconnecté\n");
 		    exit(0);
