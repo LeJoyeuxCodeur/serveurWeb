e73b50f8dd73725bbbb500436efde09190cd05d4
ANTOINE DERANTON <derantoa@frene13.iut-infobio.priv.univ-lille1.fr>
2015-02-25 09:32:55 +0100

    http

--- webserver/main.c
+++ webserver/main.c
@@ -13,15 +13,19 @@ int main(void){
 	int client;
 	int pid;
 	FILE *file;
+	int version1, version2;
 	const char *intro = "Bienvenue, mortel, sur ce magnifique serveur d'echo\nIci ma gueule, quand tu parles, on te répond !\nCependant, tu auras la grosse impression de dialoguer... tout seul !\nHeureusement, tu connais ça, espèce de sans amis!\nTu peux t'en aller après avoir pris assez de vents dans la tête avec un ctrl + c\nSalut rémi sans amis!\n";
 	char buffer[1024];
 	
+	char* reqS1, reqS2;
+	int reqI1, reqI2;
 	while(1){
 		if((client = accept(serveur, NULL, NULL)) == -1)
 			perror("accept");
 		printf("Client connecté\n");
 		fflush(stdout);
-		write(client, intro, strlen(intro));
+		//write(client, intro, strlen(intro));
+		printf("%s",intro);
 		if((pid=fork()) == -1){
 		  perror("fork");
 		}else{
@@ -31,7 +35,13 @@ int main(void){
 		  }else{
 		    file=fdopen(client, "w+");
 		    while(fgets(buffer, 1024, file)!=NULL){
-		      fprintf(file, "YouCantTestMyServer %s" ,buffer);
+		      if(buffer[0]=='G' && buffer[1]=='E' && buffer[2]=='T'){
+			sscanf(buffer, "%s %s HTTP%d.%d", reqS1, reqS2 ,reqI1 ,reqI2);
+			if(reqI1==1 && (reqI2 == 1 || reqI2==0)){
+			}
+		      }
+		      //fprintf(file, "YouCantTestMyServer %s" ,buffer);
+		      printf("YouCantTestMyServer %s" ,buffer);
 		    }
 		    printf("Client déconnecté\n");
 		    exit(0);
